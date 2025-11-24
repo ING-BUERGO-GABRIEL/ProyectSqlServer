fa434a4c71220d2436466d9b121f343e3cf91ca0
ING-BUERGO-GABRIEL <156217849+ING-BUERGO-GABRIEL@users.noreply.github.com>
2025-11-24 00:28:11 -0400

    Add DimBook package and expand book change data

--- BookstoreOLTP/Schema/Programmability/Stored Procedures/GetBookChangesByRowVersion.sql
+++ BookstoreOLTP/Schema/Programmability/Stored Procedures/GetBookChangesByRowVersion.sql
@@ -5,15 +5,22 @@ CREATE PROCEDURE [dbo].[GetBookChangesByRowVersion]
 )
 AS
 BEGIN
-        SELECT [book_id]
-                  ,[title]
-                  ,[isbn13]
-                  ,[language_id]
-                  ,[num_pages]
-                  ,[publication_date]
-                  ,[publisher_id]
-          FROM [dbo].[book]
-          WHERE [rowversion] > CONVERT(ROWVERSION,@startRow)
-          AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+SELECT b.[book_id]
+          ,b.[title]
+          ,b.[isbn13]
+          ,b.[language_id]
+          ,l.[language_code]
+          ,l.[language_name]
+          ,b.[num_pages]
+          ,b.[publication_date]
+          ,b.[publisher_id]
+          ,p.[publisher_name]
+  FROM [dbo].[book] b
+  LEFT JOIN [dbo].[book_language] l
+    ON l.[language_id] = b.[language_id]
+  LEFT JOIN [dbo].[publisher] p
+    ON p.[publisher_id] = b.[publisher_id]
+  WHERE b.[rowversion] > CONVERT(ROWVERSION,@startRow)
+  AND b.[rowversion] <= CONVERT(ROWVERSION,@endRow)
 END
 GO
