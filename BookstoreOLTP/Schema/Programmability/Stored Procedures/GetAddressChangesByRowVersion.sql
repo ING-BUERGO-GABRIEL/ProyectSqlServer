185cc03a0b05e9acefe9595f7ef9d0b62a1309d8
ING-BUERGO-GABRIEL <gabrielbuergo@gmail.com>
2025-11-24 21:05:40 -0400

    pROCEDURE

--- BookstoreOLTP/Schema/Programmability/Stored Procedures/GetAddressChangesByRowVersion.sql
+++ BookstoreOLTP/Schema/Programmability/Stored Procedures/GetAddressChangesByRowVersion.sql
@@ -1,17 +1,40 @@
 CREATE PROCEDURE [dbo].[GetAddressChangesByRowVersion]
 (
-   @startRow BIGINT
-   ,@endRow  BIGINT
+    @startRow BIGINT,
+    @endRow   BIGINT
 )
 AS
 BEGIN
-        SELECT [address_id]
-                  ,[street_number]
-                  ,[street_name]
-                  ,[city]
-                  ,[country_id]
-          FROM [dbo].[address]
-          WHERE [rowversion] > CONVERT(ROWVERSION,@startRow)
-          AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+    SET NOCOUNT ON;
+
+    SELECT 
+        a.[address_id],
+        a.[street_number],
+        a.[street_name],
+        a.[city],
+        a.[country_id],
+        co.[country_name]           AS CountryName,
+        ca.[customer_id],
+        ca.[status_id]              AS AddressStatusID,
+        ads.[address_status]        AS AddressStatus
+    FROM [dbo].[address] a
+    LEFT JOIN [dbo].[country] co 
+        ON co.country_id = a.country_id
+    LEFT JOIN [dbo].[customer_address] ca 
+        ON ca.address_id = a.address_id
+    LEFT JOIN [dbo].[address_status] ads 
+        ON ads.status_id = ca.status_id
+    WHERE
+        (a.[rowversion] > CONVERT(ROWVERSION, @startRow) 
+         AND a.[rowversion] <= CONVERT(ROWVERSION, @endRow))
+    OR 
+        (co.[rowversion] > CONVERT(ROWVERSION, @startRow) 
+         AND co.[rowversion] <= CONVERT(ROWVERSION, @endRow))
+    OR 
+        (ca.[rowversion] > CONVERT(ROWVERSION, @startRow) 
+         AND ca.[rowversion] <= CONVERT(ROWVERSION, @endRow))
+    OR
+        (ads.[rowversion] > CONVERT(ROWVERSION, @startRow) 
+         AND ads.[rowversion] <= CONVERT(ROWVERSION, @endRow));
 END
 GO
