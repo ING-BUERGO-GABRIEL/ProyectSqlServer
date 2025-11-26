0ab18198f7415bb2def9cf8e65ba5006a5c25b3b
ING-BUERGO-GABRIEL <gabrielbuergo@gmail.com>
2025-11-26 01:09:40 -0400

    Subir

--- BookstoreDW/Schema/Programmability/Stored Procedures/DW_MergeDimAddress.sql
+++ BookstoreDW/Schema/Programmability/Stored Procedures/DW_MergeDimAddress.sql
@@ -1,49 +1,16 @@
 CREATE PROCEDURE [dbo].[DW_MergeDimAddress]
 AS
 BEGIN
-        SET NOCOUNT ON;
-
-        MERGE [dbo].[DimAddress] AS target
-        USING (
-                SELECT [AddressID]
-                      ,[StreetNumber]
-                      ,[StreetName]
-                      ,[City]
-                      ,[CountryID]
-                      ,[CountryName]
-                      ,[AddressStatusID]
-                      ,[AddressStatus]
-                FROM [staging].[DimAddress]
-        ) AS source
-                ON target.[AddressID] = source.[AddressID]
-        WHEN MATCHED THEN
-                UPDATE SET [StreetNumber]     = source.[StreetNumber]
-                         ,[StreetName]       = source.[StreetName]
-                         ,[City]             = source.[City]
-                         ,[CountryID]        = source.[CountryID]
-                         ,[CountryName]      = source.[CountryName]
-                         ,[AddressStatusID]  = source.[AddressStatusID]
-                         ,[AddressStatus]    = source.[AddressStatus]
-        WHEN NOT MATCHED THEN
-                INSERT (
-                        [AddressID]
-                       ,[StreetNumber]
-                       ,[StreetName]
-                       ,[City]
-                       ,[CountryID]
-                       ,[CountryName]
-                       ,[AddressStatusID]
-                       ,[AddressStatus]
-                )
-                VALUES (
-                        source.[AddressID]
-                       ,source.[StreetNumber]
-                       ,source.[StreetName]
-                       ,source.[City]
-                       ,source.[CountryID]
-                       ,source.[CountryName]
-                       ,source.[AddressStatusID]
-                       ,source.[AddressStatus]
-                );
+         UPDATE da
+        SET [StreetNumber]    = sa.[StreetNumber]
+           ,[StreetName]      = sa.[StreetName]
+           ,[City]            = sa.[City]
+           ,[CountryID]       = sa.[CountryID]
+           ,[CountryName]     = sa.[CountryName]
+           ,[AddressStatusID] = sa.[AddressStatusID]
+           ,[AddressStatus]   = sa.[AddressStatus]
+        FROM [dbo].[DimAddress]        AS da
+        INNER JOIN [staging].[DimAddress] AS sa
+                ON da.[AddressID] = sa.[AddressID];
 END
 GO
--- BookstoreOLTP/Schema/Programmability/Stored Procedures/GetCustomerChangesByRowVersion.sql
+++ BookstoreOLTP/Schema/Programmability/Stored Procedures/GetCustomerChangesByRowVersion.sql
@@ -5,12 +5,39 @@ CREATE PROCEDURE [dbo].[GetCustomerChangesByRowVersion]
 )
 AS
 BEGIN
-        SELECT [customer_id]
-                  ,[first_name]
-                  ,[last_name]
-                  ,[email]
-          FROM [dbo].[customer]
-          WHERE [rowversion] > CONVERT(ROWVERSION,@startRow)
-          AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+      SET NOCOUNT ON;
+
+    /*
+        Returns customer and address attributes for rows whose rowversion changed
+        in customer, customer_address, address, country, or address_status.
+        Designed to feed BookstoreDW staging.DimCustomer.
+    */
+    SELECT DISTINCT
+           c.[customer_id]          AS [CustomerID],
+           c.[first_name]           AS [FirstName],
+           c.[last_name]            AS [LastName],
+           c.[email]                AS [Email],
+           ca.[address_id]          AS [AddressID],
+           ca.[status_id]           AS [AddressStatusID],
+           a.[street_number]        AS [StreetNumber],
+           a.[street_name]          AS [StreetName],
+           a.[city]                 AS [City],
+           co.[country_name]        AS [CountryName],
+           ads.[address_status]     AS [AddressStatus]
+    FROM [dbo].[customer] c
+    LEFT JOIN [dbo].[customer_address] ca
+        ON ca.[customer_id] = c.[customer_id]
+    LEFT JOIN [dbo].[address] a
+        ON a.[address_id] = ca.[address_id]
+    LEFT JOIN [dbo].[country] co
+        ON co.[country_id] = a.[country_id]
+    LEFT JOIN [dbo].[address_status] ads
+        ON ads.[status_id] = ca.[status_id]
+    WHERE
+        (c.[rowversion]  > CONVERT(ROWVERSION, @startRow) AND c.[rowversion]  <= CONVERT(ROWVERSION, @endRow)) OR
+        (ca.[rowversion] > CONVERT(ROWVERSION, @startRow) AND ca.[rowversion] <= CONVERT(ROWVERSION, @endRow)) OR
+        (a.[rowversion]  > CONVERT(ROWVERSION, @startRow) AND a.[rowversion]  <= CONVERT(ROWVERSION, @endRow)) OR
+        (co.[rowversion] > CONVERT(ROWVERSION, @startRow) AND co.[rowversion] <= CONVERT(ROWVERSION, @endRow)) OR
+        (ads.[rowversion]> CONVERT(ROWVERSION, @startRow) AND ads.[rowversion]<= CONVERT(ROWVERSION, @endRow));
 END
 GO
